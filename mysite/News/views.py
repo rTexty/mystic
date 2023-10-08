d55ce1cbb6e47c12e1b2d935c388c2dd425ef651
rTexty <kamil.axmadiev88@gmail.com>
2023-10-08 20:34:07 +0500

    asd

--- mysite/News/views.py
+++ mysite/News/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from .models import News
+
+def index (request):
+    news = News. objects.order_by('-created_at')
+    context = {
+    'news': news,
+    'title': 'Список новостей'
+    }
+    return render(request, template_name='news/index.html', context= context)
