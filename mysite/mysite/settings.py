bd5281872a1c80e19b176edec28dbcda61ebba14
rTexty <kamil.axmadiev88@gmail.com>
2023-10-08 23:55:31 +0500

    Finally done

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'MySite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'site',
+        'USER': 'rtexty',
+        'PASSWORD': 'rtexty',
+        'HOST': '127.0.0.1',
+        'PORT': '5555',
     }
 }
 
@@ -122,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
