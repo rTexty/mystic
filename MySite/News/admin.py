edeb222ac6f310de301c87eb14ed5a2dfd27624e
rTexty <kamil.axmadiev88@gmail.com>
2023-10-08 19:39:57 +0500

    almost

--- MySite/News/admin.py
+++ MySite/News/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'created_at', 'updated_at', 'photo', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'created_at',)
+
+
+admin.site.register(models.News, NewsAdmin)
+
--- MySite/News/models.py
+++ MySite/News/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class News(models.Model) :
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photos/88/8m/8d/')
+    is_published = models.BooleanField(default=True)
+    
+    
+    def _str_(self) :
+        return self.title
+    
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at']
